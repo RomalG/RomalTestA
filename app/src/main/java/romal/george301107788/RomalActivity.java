93b453b5d952997b3310cb26fad954168b29dbca
Romal George <romalgeorge3@gmail.com>
2020-10-31 08:52:50 -0400

    Initial commit

---
+++ app/src/main/java/romal/george301107788/RomalActivity.java
@@ -0,0 +1,14 @@
+package romal.george301107788;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RomalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
